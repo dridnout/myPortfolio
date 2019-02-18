afb38b408a6aea0e3f306c1f1d06e5de4b02cb3f
Alexander Zalomanin <dridnout@yandex.ru>
2019-02-18 15:59:22 +0400

    update

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,29 @@
 $(document).ready(function() {
 
+	$(".menu-btn__button").click(function(){
+		$(".menu-btn__button-span").toggleClass("button-toggle");
+		
+
+		
+		var btnText = $(".menu-btn__text"); 
+		var check = btnText.text();
+
+		// Меняем текст
+		setTimeout(function(){
+			if(check === "Меню"){
+				btnText.text("Закрыть")
+			}else{
+				btnText.text("Меню")
+			}
+		}, 100);
+
+		// Добавляем/удаляем класс
+		btnText.addClass("menu-btn__text_splash");
+		setTimeout(function() {
+			btnText.removeClass("menu-btn__text_splash")
+		}, 600);
+
+	})
 	
 
 });
